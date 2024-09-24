ce6fc4d7deca39e48717a150b07d0f5a8e596229
xMirnax <mirna.tominac@gmail.com>
2024-09-24 12:58:01 +0200

    Grid Coordinate System

---
+++ Assets/Scripts/GridCoords/GridCoordsSystemTest.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class GridCoordsSystemTest : MonoBehaviour
+{
+    private GridConfig gridConfig;
+    public Color lineColor = Color.white;
+
+    void Start()
+    {
+        gridConfig = new GridConfig(10f, 10f, 3);
+        DrawGrid();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2Int boardCoords = GridCoordsUtility.GetBoardCoords(gridConfig, mousePosition);
+
+            Debug.Log($"Clicked at Board Position: ({boardCoords.x}, {boardCoords.y})");
+        }
+    }
+
+    public void DrawGrid()
+    {
+        for (int ring = 0; ring < gridConfig.numberOfRings; ring++)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Vector2Int coords = new Vector2Int(ring, i);
+                Vector2 worldPos = GridCoordsUtility.GetWorldCoords(coords, gridConfig);
+
+                Vector2Int boardCoords = GridCoordsUtility.GetBoardCoords(gridConfig, worldPos);
+
+                Debug.Log($"Grid: ({ring},{i}) -> World: {worldPos} -> Board: {boardCoords}");
+
+                string positionText = $"Grid: {ring},{i}\nBoard: {boardCoords.x},{boardCoords.y}";
+                CreateWorldText(transform, positionText, worldPos, 30, Color.white, TextAnchor.MiddleCenter, TextAlignment.Center, 0);
+
+                Vector2Int nextCoords = new Vector2Int(ring, (i + 1) % 8);
+                Vector2 nextWorldPos = GridCoordsUtility.GetWorldCoords(nextCoords, gridConfig);
+
+                DrawLineInGameView(worldPos, nextWorldPos, lineColor);
+            }
+        }
+    }
+
+    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+
+    private void DrawLineInGameView(Vector3 start, Vector3 end, Color color)
+    {
+        GameObject lineObject = new GameObject("Line");
+        LineRenderer lineRenderer = lineObject.AddComponent<LineRenderer>();
+
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+
+        lineRenderer.startWidth = 1f;
+        lineRenderer.endWidth = 1f;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+
+        lineRenderer.sortingOrder = 1;
+    }
+}
---
+++ Assets/Scripts/GridCoords/GridCoordsUtility.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public struct GridConfig
+{
+    public float gridSize;
+    public float ringGap;
+    public int numberOfRings;
+
+    public GridConfig(float gridSize = 5f, float ringGap = 5, int numberOfRings = 3)
+    {
+        this.gridSize = gridSize;
+        this.ringGap = ringGap;
+        this.numberOfRings = numberOfRings;
+    }
+}
+
+public class GridCoordsUtility
+{
+
+    private static readonly Vector2[] directions = new Vector2[]
+    {
+        new Vector2(-1, 1), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(1, 0), new Vector2(1, -1), new Vector2(0, -1),
+        new Vector2(-1, -1), new Vector2(-1, 0)
+    };
+
+    public static Vector2Int GetBoardCoords(GridConfig gridConfig, Vector2 worldCoords)
+    {
+        Vector2Int closestCoords = new Vector2Int(-1, -1); // Default value if no valid coords found
+        float minDistance = float.MaxValue;
+
+        for (int ring = 0; ring < gridConfig.numberOfRings; ring++)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Vector2Int coords = new Vector2Int(ring, i);
+                Vector2 worldPos = GridCoordsUtility.GetWorldCoords(coords, gridConfig);
+
+                float distance = Vector2.Distance(worldCoords, worldPos);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestCoords = coords;
+                }
+            }
+        }
+
+        if(minDistance > 20)
+        {
+            return new Vector2Int(-1, -1);
+        }
+        return closestCoords;
+    }
+
+
+    public static Vector2 GetWorldCoords(Vector2Int boardCoords, GridConfig gridConfig)
+    {
+        int ring = boardCoords.x;
+        int position = boardCoords.y;
+
+        float ringSize = (ring + 1) * (2.5f + gridConfig.ringGap); 
+        Vector2 direction = directions[position];
+        return direction * ringSize;
+    }
+}
